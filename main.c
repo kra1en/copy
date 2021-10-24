1a6c0157d75ab277266377f35f3f9286a7004126
Tema <im-unstoppable@mail.ru>
2021-10-24 13:43:48 +0300

    added realization

--- main.c
+++ main.c
@@ -1,5 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+void main(int argc , char **argv)
+{
+    if (argc != 3)
+        printf("You need to enter two arguments");
+    else
+    {
+        FILE *first_file, *second_file;
+        char c;
 
-int main(){
-    printf("Hello, World!");
+        first_file = fopen(argv[1],"r");
+        second_file = fopen(argv[2],"w");
+
+        
+        while((c = fgetc(first_file)) !=EOF)
+            fputc(c, second_file);
+        
+        fclose(first_file);
+        fclose(second_file);
+    }
+
 }
